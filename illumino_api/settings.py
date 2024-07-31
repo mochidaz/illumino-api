c558b6ca5acde647beddb63d8030483528ed7d34
Rahman Hakim <rahmanhakim2435@gmail.com>
2024-07-31 15:12:27 +0700

    stuff

--- illumino_api/settings.py
+++ illumino_api/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'illumino_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': os.getenv('DATABASE_ENGINE'),
         'NAME': os.getenv('DATABASE_NAME'),
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
