20146ebd2cc3e71306ea91d22c4de6a711fbec3b
Rahman Hakim <rahmanhakim2435@gmail.com>
2024-07-31 15:28:14 +0700

    allowed host

--- illumino_api/settings.py
+++ illumino_api/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-kmce*^vg^h00=s&u$qbh@cuyrgrht6e91hidh%m9^oxix$kw)y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
