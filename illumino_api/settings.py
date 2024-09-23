9538383a617fee12c7f8b9257cfc4ed0a705021f
Rahman Hakim <rahmanhakim2435@gmail.com>
2024-09-23 20:24:37 +0700

    stuff

--- illumino_api/settings.py
+++ illumino_api/settings.py
@@ -56,10 +56,10 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -84,9 +84,20 @@ TEMPLATES = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',
+]
+
 CORS_ALLOW_HEADERS = (
     '*'
 )
+
+CORS_ALLOW_CREDENTIALS = True
 
 WSGI_APPLICATION = 'illumino_api.wsgi.application'
 
