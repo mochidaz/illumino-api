d99bbe452bcfcf5844a47bbb864e84bbf336166b
Rahman Hakim <rahmanhakim2435@gmail.com>
2024-09-27 19:40:40 +0700

    stuff

--- journal/models.py
+++ journal/models.py
@@ -7,6 +7,7 @@ class Journal(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255, null=True, blank=True)
     content = models.TextField()
+    date = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
